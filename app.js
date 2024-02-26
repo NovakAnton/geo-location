f7427bd6db2e116c6b8c33d4182513051755e715
NovakAnton <157126729+NovakAnton@users.noreply.github.com>
2024-02-26 19:54:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ let lat = undefined;
 let lon = undefined;
 const button = document.querySelector("#locateBtn");
 
+function toCapitalizedCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
 button.addEventListener("click", () => {
   lat = document.querySelector("#latitudeInput").value;
   // document.querySelector("#latitudeInput").value = "";
@@ -25,11 +33,11 @@ function displayGeoLocation(lat, lon) {
       }
     })
     .then((jsonResponse) => {
-      let city = jsonResponse.address.city;
-      let country = jsonResponse.address.country;
-      let suburb = jsonResponse.address.suburb;
-      let road = jsonResponse.address.road;
-      let house_number = jsonResponse.address.house_number;
+      let city = toCapitalizedCase(jsonResponse.address.city);
+      let country = toCapitalizedCase(jsonResponse.address.country);
+      let suburb = toCapitalizedCase(jsonResponse.address.suburb);
+      let road = toCapitalizedCase(jsonResponse.address.road);
+      let house_number = toCapitalizedCase(jsonResponse.address.house_number);
       document.querySelector("#coordinates").textContent =
         city + ", " + country;
       document.querySelector("#details").textContent =
